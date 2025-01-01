050eb4eed42574e97ef0aada6259d652c2dfd833
shishirRsiam <shishir.siam01@gmail.com>
2025-01-02 02:01:41 +0600

    Account Activation error fix done

--- _3_NextHire_Job_Board_Project/src/Component/Authentication/AccountActivation.jsx
+++ _3_NextHire_Job_Board_Project/src/Component/Authentication/AccountActivation.jsx
@@ -12,9 +12,7 @@ function ActivationPage() {
 
     const activateAccount = async () => {
         try {
-        const api = `${API.ActivationAPI}${id}/${token}`;
-
-        const response = await fetch(api);
+        const response = await fetch(`http://localhost:8000/api/accounts/activate/${id}/${token}/`);
         const data = await response.json();
 
         if (response.ok) {
